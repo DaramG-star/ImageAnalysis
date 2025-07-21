f7da3e5adef3e3cb289eb1d184bb7c73a6d29c54
DARAMG <sukrladuswn@naver.com>
2025-07-21 09:16:20 +0900

    7/20 commit

--- my-gesture-project/backend/gesture/routing.py
+++
@@ -1,9 +0,0 @@
-# backend/gesture/routing.py
-
-from django.urls import path # re_path 대신 path를 import
-from . import consumers
-
-websocket_urlpatterns = [
-    # 정규표현식(re_path) 대신 간단한 path로 변경
-    path('ws/gesture/', consumers.GestureConsumer.as_asgi()),
-]
